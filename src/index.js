4d6e95cbcf3aded10fcf0537526a03a7bbb56e15
jrosi <j.rosimo098@gmail.com>
2022-12-15 12:43:51 +0800

    Feat: added build folder for deployment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <AuthContextProvider>
         <OccupantsContextProvider>
           <App />
